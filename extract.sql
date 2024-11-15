189099f97585c71ba273205579297053e43f5c77
SIMRANJIT KAUR <skaur@ymsl.in>
2024-11-15 13:11:36 +0530

    deploy + extract update

--- extract.sql
+++ extract.sql
@@ -9,6 +9,17 @@ CREATE OR ALTER WAREHOUSE SNOW_WH
 -- Separate database for git repository
 CREATE OR ALTER DATABASE SnowflakeTest;
 
+-- API integration is needed for GitHub integration
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/Simran-git2604') 
+  ENABLED = TRUE;
+
+
+-- Git repository object is similar to external stage
+CREATE OR REPLACE GIT REPOSITORY SnowflakeTest.public.repo
+  API_INTEGRATION = git_api_integration
+  ORIGIN = 'https://github.com/Simran-git2604/Snowflake'; 
 -- DB level objects
 CREATE OR ALTER SCHEMA bronze;
 
